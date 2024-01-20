a41900fff1d6b7654190583d187460d3f296bde2
Bkurtuldu <91678995+Bkurtuldu@users.noreply.github.com>
2024-01-20 15:07:47 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,22 @@ function App() {
 
     const [data, setData] = useState([]);
 
+    const [options, setOptions] = useState([]);
+
+    useEffect(() => {
+        const fetchDataFromDatabase = async () => {
+            try {
+                const response = await fetch(`http://localhost:3001/api/data?number=13`);
+                const data = await response.json();
+                setOptions(data);
+                console.log(options);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchDataFromDatabase();
+    }, []);
 
 
     const fetchData = async (numberParam, artistName = "", songGenre = "") => {
@@ -41,16 +57,17 @@ function App() {
 
 
     const handleSubmit = (event) => {
+
         event.preventDefault();
-        // You can perform actions with the input value here
 
         fetchData(12, nameOfUser, musicGenre);
 
     };
 
     const handleInsertSong = (event) => {
+
+
         event.preventDefault();
-        // You can perform actions with the input value here
 
         fetchData();
 
@@ -61,13 +78,13 @@ function App() {
 
         <div className="App">
             <header className="App-header" style={{
-                background: 'transparent', // Gradient background
-                textAlign: 'center',  // Center align text
-                padding: '10px',      // Add some padding
-                color: 'white',       // Text color
-                fontFamily: 'Comic Sans MS, cursive', // Apply the custom font
-                fontWeight: 'bold',   // Adjust font weight if needed
-                fontSize: '22px',     // Adjust font size if needed
+                background: 'transparent', 
+                textAlign: 'center',  
+                padding: '10px',      
+                color: 'white',       
+                fontFamily: 'Comic Sans MS, cursive', 
+                fontWeight: 'bold',   
+                fontSize: '22px',     
             }}>
 
 
@@ -104,7 +121,7 @@ function App() {
                                 fontFamily: 'Comic Sans MS, cursive',
                                 fontWeight: 900,
                                 fontSize: '20px',
-                                // fontStyle: 'italic',  // Apply italic style
+                                
                             }}
                         >
                             Return information about artist and the total "Pop" song count of them
@@ -132,7 +149,7 @@ function App() {
                                 fontFamily: 'Comic Sans MS, cursive',
                                 fontWeight: 900,
                                 fontSize: '20px',
-                                // fontStyle: 'italic',  // Apply italic style
+                               
                             }}
                         >
                             Artists who were born after '2000'
@@ -160,7 +177,7 @@ function App() {
                                 fontFamily: 'Comic Sans MS, cursive',
                                 fontWeight: 900,
                                 fontSize: '20px',
-                                // fontStyle: 'italic',  // Apply italic style
+                                
                             }}
                         >
                             List of favourite artists and count of users who have favourited them
@@ -188,7 +205,7 @@ function App() {
                                 fontFamily: 'Comic Sans MS, cursive',
                                 fontWeight: 900,
                                 fontSize: '20px',
-                                // fontStyle: 'italic',  // Apply italic style
+                                
                             }}
                         >
                             The count of female users for each favorite genre
@@ -216,7 +233,7 @@ function App() {
                                 fontFamily: 'Comic Sans MS, cursive',
                                 fontWeight: 900,
                                 fontSize: '20px',
-                                // fontStyle: 'italic',  // Apply italic style
+                                
                             }}
                         >
                             The count of users who were born after the year 2000 for each favorite artist
@@ -244,7 +261,7 @@ function App() {
                                 fontFamily: 'Comic Sans MS, cursive',
                                 fontWeight: 900,
                                 fontSize: '20px',
-                                // fontStyle: 'italic',  // Apply italic style
+                                
                             }}
                         >
                             The most liked songs along with the total number of likes for each song
@@ -272,7 +289,7 @@ function App() {
                                 fontFamily: 'Comic Sans MS, cursive',
                                 fontWeight: 900,
                                 fontSize: '20px',
-                                // fontStyle: 'italic',  // Apply italic style
+                                
                             }}
                         >
                             The most liked songs in the 'Pop' category along with the total number of likes for each song (from all songs)
@@ -303,7 +320,7 @@ function App() {
                         fontFamily: 'Comic Sans MS, cursive',
                         fontWeight: 900,
                         fontSize: '15px',
-                        // fontStyle: 'italic',  // Apply italic style
+                        
                     }}>
                         Name:
                         <input type="text" value={nameOfUser} onChange={(e) => setName(e.target.value)} />
@@ -317,7 +334,7 @@ function App() {
                             fontFamily: 'Comic Sans MS, cursive',
                             fontWeight: 900,
                             fontSize: '15px',
-                            // fontStyle: 'italic',  // Apply italic style
+                            
                         }}>
                             Music Genre:
                             <select value={musicGenre} defaultValue="" onChange={(e) => setMusicGenre(e.target.value)}>
@@ -374,7 +391,7 @@ function App() {
                         fontFamily: 'Comic Sans MS, cursive',
                         fontWeight: 900,
                         fontSize: '15px',
-                        // fontStyle: 'italic',  // Apply italic style
+                        
                     }}>
                         Song Name:
                         <input type="text" value={songToInsert} onChange={(e) => setSongToInsert(e.target.value)} />
@@ -388,7 +405,7 @@ function App() {
                             fontFamily: 'Comic Sans MS, cursive',
                             fontWeight: 900,
                             fontSize: '15px',
-                            // fontStyle: 'italic',  // Apply italic style
+                            
                         }}>
                             Music Genre of Song:
                             <select value={songGenreToInsert} defaultValue="" onChange={(e) => setSongGenreToInsert(e.target.value)}>
@@ -429,13 +446,17 @@ function App() {
                             fontFamily: 'Comic Sans MS, cursive',
                             fontWeight: 900,
                             fontSize: '15px',
-                            // fontStyle: 'italic',  // Apply italic style
+                            
                         }}>
                             Album of Song:
                             <select value={albumToInsert} defaultValue="" onChange={(e) => setAlbumToInsert(e.target.value)}>
 
-                               
-
+                                {options.map((option, index) => (
+                                    <option key={index} value={option.albName}>
+                                        {option.albName}
+                                    </option>
+                                ))}
+                                
 
                             </select>
                         </label>
